ecbb236d0bc4807d8ec38e407eac5d3b9b8c242c
Queble <queblegamedevelopment@gmail.com>
2024-08-19 13:58:49 -0500

    cleaned up organization

--- navigation_server_example/navigator_entity/navigator_entity.gd
+++ navigation_server_example/navigator_entity/navigator_entity.gd
@@ -1,30 +1,25 @@
 extends CharacterBody2D
 
-# EXPORTS
-# -----------------------------------------------------------------------------
 ## The movement speed of the entity
-@export var movement_speed: float = 3000.0
+@export var movement_speed: float = 8000.0
 ## The target node to move to
 @export var movement_target: Node2D
-# -----------------------------------------------------------------------------
 
-# ONREADY
-# -----------------------------------------------------------------------------
 ## Reference to the navigation agent node.
 ## A NavigationAgent2D node must be added to your scene and referenced here in order-
 ## to communicate with the navigation server.
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
-
 ## Reference to the recalculation timer.
 @onready var recalc_timer : Timer = $RecalcTimer
-# -----------------------------------------------------------------------------
 
-# FUNCTIONS
-# -----------------------------------------------------------------------------
+var on_nav_link : bool = false
+var nav_link_end_position : Vector2
+
 func _ready():
 	# Connect signals
 	recalc_timer.timeout.connect(_on_recalc_timer_timeout)
 	navigation_agent.link_reached.connect(_on_navigation_link_reached)
+	navigation_agent.waypoint_reached.connect(_on_waypoint_reached)
 
 	# These values need to be adjusted according to the actor's speed, -
 	# the navigation layout, and the actor's collision shape.
@@ -50,21 +45,30 @@ func _physics_process(delta):
 	velocity = current_agent_position.direction_to(next_path_position) * movement_speed * delta
 	move_and_slide()
 
+## Setup the navigation agent.
 func actor_setup():
 	# Wait for the first physics frame so the NavigationServer can sync.
 	await get_tree().physics_frame
-	recalc_target_position()
+	set_target_position(movement_target.position)
 
-func recalc_target_position() -> void:
-	if !movement_target: printerr("Movement target not set!"); return # Breaks if movement_target is null
-	navigation_agent.target_position = movement_target.global_position
+## Set the target position of the navigation agent.
+func set_target_position(target_position : Vector2 = Vector2.ZERO) -> void:
+	navigation_agent.target_position = target_position
 
 ## Called when the recalculation timer times out.
 func _on_recalc_timer_timeout() -> void:
-	recalc_target_position()
+	if not on_nav_link:
+		set_target_position(movement_target.position)
 
 ## Called when a navigation link has been reached.
 func _on_navigation_link_reached(details : Dictionary) -> void:
-	print("Navigation link reached: ", details)
-# -----------------------------------------------------------------------------
+	on_nav_link = true # Temporarily disable recalculation to prevent jittering.
+	nav_link_end_position = details.link_exit_position
 
+## Called when a waypoint has been reached.
+func _on_waypoint_reached(details : Dictionary) -> void:
+	# This next line checks the distance to the waypoint with a threshhold.
+	# If the distance is less than 5.0, then the waypoint has been reached.
+	# This check produces unexpected results when comparing vectors directly.
+	if details.position.distance_to(nav_link_end_position) < 5.0:
+		on_nav_link = false
